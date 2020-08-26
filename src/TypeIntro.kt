3fb3a7a6f138a6559af4b74c233d8f5dc45b2361
Abdulqader <alhemyariabdulqader@gmail.com>
2020-08-26 21:52:43 +0300

    First assignment on Kotlin

---
+++ src/TypeIntro.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    var experiencePoint:Int=5;
+
+    val playerName:String="Estragon";
+
+    experiencePoint=experiencePoint+5
+
+    println(experiencePoint)
+
+    println(playerName)
+
+    var hasSteed:Boolean=false;
+
+    println(playerName.reversed())
+}
